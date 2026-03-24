fd6cbf13b4279b899227be8dd5c12a4790eb6f5f
Yashverm <66511368+Yashverm@users.noreply.github.com>
2026-03-24 10:01:32 +0530

    idk14.cpp

--- FOCP-II Lab Manual/C-C++ practice/c++/idk14.cpp
+++ FOCP-II Lab Manual/C-C++ practice/c++/idk14.cpp
@@ -6,7 +6,7 @@ private:
     float base, height, area;
 
 public:
-    // Constructor
+ 
     Triangle() {
         cout << "Enter base of triangle: ";
         cin >> base;
@@ -15,21 +15,21 @@ public:
         cin >> height;
 
         area = 0.5 * base * height;
-    }//fahhhhh
+    }
 
-    // Function to display area
+    
     void display() {
         cout << "Area of Triangle = " << area << endl;
     }
 
-    // Destructor
+  
     ~Triangle() {
         cout << "Destructor called. Triangle object destroyed." << endl;
     }
 };
 
 int main() {
-    Triangle t;   // Constructor automatically called
-    t.display();  // Display area
+    Triangle t;   
+    t.display(); 
     return 0;
 }
