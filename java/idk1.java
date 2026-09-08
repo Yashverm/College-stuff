08b09704f46c413fd8cf56ae400cae6539e36f41
Yashverm <66511368+Yashverm@users.noreply.github.com>
2026-09-08 13:42:44 +0530

    Add files via upload

---
+++ java/idk1.java
@@ -0,0 +1,21 @@
+public class idk1 {
+    public static void main(String[] args) {
+        int sem = 4;
+        int addyear = 2024;
+        String rollNo = "25csu276";
+        long mobNo = 9876543210L;
+        float attndperc = 88.5f;
+        double cgpa = 8.75;
+        char section = 'B';
+        boolean isHostel = true;
+
+        System.out.println("Semester: " + sem);
+        System.out.println("Admission Year: " + addyear);
+        System.out.println("Roll Number: " + rollNo);
+        System.out.println("Mobile Number: " + mobNo);
+        System.out.println("Attendance: " + attndperc + "%");
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Section: " + section);
+        System.out.println("Hostel Status: " + isHostel);
+    }
+}
